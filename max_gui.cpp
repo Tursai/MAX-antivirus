6261494d142743442b71d15234c550f4991cee9c
Tursai <127190459+Tursai@users.noreply.github.com>
2026-06-22 20:03:21 +0500

    Refactor max_gui.cpp for UI enhancements
    Updated window class name and dimensions, added background color, and improved path handling for HTML file.

--- max_gui.cpp
+++ max_gui.cpp
@@ -1,46 +1,53 @@
 #include <windows.h>
 #include <string>
 #include <wrl.h>
+#include <shlwapi.h>
 #include "WebView2.h"
 
+#pragma comment(lib, "shlwapi.lib")
 using namespace Microsoft::WRL;
 
 HWND hWnd;
 ComPtr<ICoreWebView2Controller> webviewController;
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    if (message == WM_SIZE && webviewController != nullptr) {
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+    if (msg == WM_SIZE && webviewController) {
         RECT bounds; GetClientRect(hWnd, &bounds);
         webviewController->put_Bounds(bounds);
     }
-    if (message == WM_DESTROY) PostQuitMessage(0);
-    return DefWindowProc(hWnd, message, wParam, lParam);
+    if (msg == WM_DESTROY) PostQuitMessage(0);
+    return DefWindowProc(hWnd, msg, wp, lp);
 }
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
+int WINAPI WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
     WNDCLASSW wc = {0};
     wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInst;
-    wc.lpszClassName = L"MAX_WINDOW";
-    wc.hIcon = LoadIcon(hInst, IDI_APPLICATION);
+    wc.hInstance = hI;
+    wc.lpszClassName = L"MAX_UI";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     RegisterClassW(&wc);
 
-    hWnd = CreateWindowExW(0, L"MAX_WINDOW", L"MAX Antivirus", WS_OVERLAPPEDWINDOW, 
-        CW_USEDEFAULT, CW_USEDEFAULT, 1100, 750, 0, 0, hInst, 0);
-    
-    ShowWindow(hWnd, nShow);
+    hWnd = CreateWindowExW(0, L"MAX_UI", L"MAX Antivirus", WS_OVERLAPPEDWINDOW, 
+        CW_USEDEFAULT, CW_USEDEFAULT, 1200, 800, 0, 0, hI, 0);
+    ShowWindow(hWnd, nS);
 
     CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
         Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>([](HRESULT res, ICoreWebView2Environment* env) -> HRESULT {
             env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>([](HRESULT res, ICoreWebView2Controller* ctrl) -> HRESULT {
-                webviewController = ctrl;
-                ComPtr<ICoreWebView2> webview;
-                ctrl->get_CoreWebView2(&webview);
-                
-                WCHAR szPath[MAX_PATH]; GetModuleFileNameW(NULL, szPath, MAX_PATH);
-                std::wstring path(szPath);
-                std::wstring htmlPath = L"file:///" + path.substr(0, path.find_last_of(L"\\/")) + L"\\ui_mockup.html";
-                webview->Navigate(htmlPath.c_str());
+                if (ctrl) {
+                    webviewController = ctrl;
+                    ComPtr<ICoreWebView2> webview;
+                    ctrl->get_CoreWebView2(&webview);
+
+                    // Настройка путей
+                    WCHAR szPath[MAX_PATH];
+                    GetModuleFileNameW(NULL, szPath, MAX_PATH);
+                    PathRemoveFileSpecW(szPath);
+                    std::wstring htmlPath = L"file:///" + std::wstring(szPath) + L"\\ui_mockup.html";
+                    for (auto& c : htmlPath) if (c == L'\\') c = L'/';
+
+                    webview->Navigate(htmlPath.c_str());
+                }
                 return S_OK;
             }).Get());
             return S_OK;
